9d43ee7c7639d2dcc7853c259e8bc3f52b640f40
Sven Münnich <sven.muennich@viison.com>
2018-03-03 20:54:12 +0100

    Implement ClassNamespaceRule 🎉

--- src/PhpLint/Rules/ClassNamespaceRule.php
+++ src/PhpLint/Rules/ClassNamespaceRule.php
@@ -4,31 +4,38 @@ declare(strict_types=1);
 namespace PhpLint\Rules;
 
 use PhpLint\Ast\AstNode;
+use PhpLint\Ast\AstNodeType;
 use PhpLint\Linter\LintContext;
 use PhpLint\Linter\LintResult;
 
 class ClassNamespaceRule extends AbstractRule
 {
     const RULE_IDENTIFIER = 'class-namespace-rule';
+    const MESSAGE_ID_CLASS_NOT_NAMESPACED = 'classNotNamespaces';
+    const MESSAGE_ID_MULTIPLE_NAMESPACE_DEFINITIONS = 'multipleNamespaceDefinitions';
 
     public function __construct()
     {
         $this->setDescription(
             RuleDescription::forRuleWithIdentifier(self::RULE_IDENTIFIER)
                 ->explainedBy('Enforces that all classes must be contained in a PSR-4 namespace.')
+                ->usingMessageIds([
+                    self::MESSAGE_ID_CLASS_NOT_NAMESPACED,
+                    self::MESSAGE_ID_MULTIPLE_NAMESPACE_DEFINITIONS,
+                ])
                 ->rejectsExamples([
                     RuleDescription::createPhpCodeExample('class AnyClass {}'),
+                    RuleDescription::createPhpCodeExample(
+                        'namespace PhpLint\Rules;',
+                        'class AnyClass {}',
+                        'namespace PhpLint\Rules\Violation;',
+                        'class AnyOtherClass {}'
+                    ),
                 ])
                 ->acceptsExamples([
                     RuleDescription::createPhpCodeExample(
                         'namespace PhpLint\Rules;',
                         'class AnyClass {}'
-                    ),
-                    RuleDescription::createPhpCodeExample(
-                        'namespace PhpLint\Rules;',
-                        'class AnyClass {}',
-                        'namespace PhpLint\Rules\Two;',
-                        'class AnyOtherClass {}'
                     ),
                 ])
         );
@@ -40,7 +47,8 @@ class ClassNamespaceRule extends AbstractRule
     public function getTypes(): array
     {
         return [
-            'Stmt_Class',
+            AstNodeType::CLASS_DECLARATION,
+            AstNodeType::SOURCE_ROOT,
         ];
     }
 
@@ -49,6 +57,39 @@ class ClassNamespaceRule extends AbstractRule
      */
     public function validate(AstNode $node, LintContext $context, LintResult $result)
     {
-        // TODO: Implement rule validation
+        switch ($node->getType()) {
+            case AstNodeType::CLASS_DECLARATION:
+                $this->checkForClassNamespace($node, $context, $result);
+                break;
+            case AstNodeType::SOURCE_ROOT:
+                $this->checkForMultipleNamespaces($node, $context, $result);
+                break;
+        }
+    }
+
+    private function checkForClassNamespace(AstNode $node, LintContext $context, LintResult $result)
+    {
+        $parentNode = $node->getParent();
+        if (!$parentNode || $parentNode->getType() !== AstNodeType::NAMESPACE) {
+            $result->reportViolation($node, self::MESSAGE_ID_CLASS_NOT_NAMESPACED);
+        }
+    }
+
+    private function checkForMultipleNamespaces(AstNode $node, LintContext $context, LintResult $result)
+    {
+        $foundFirstNamespaceNode = false;
+        foreach ($node->getChildren() as $child) {
+            if ($child->getType() !== AstNodeType::NAMESPACE) {
+                continue;
+            }
+
+            if ($foundFirstNamespaceNode) {
+                // nth 'namespace' node
+                $result->reportViolation($node, self::MESSAGE_ID_MULTIPLE_NAMESPACE_DEFINITIONS);
+            } else {
+                // First 'namespace' node
+                $foundFirstNamespaceNode = true;
+            }
+        }
     }
 }
