dfde336dfc19add215a6c609f6e0b624c8452b59
Sven Münnich <sven.muennich@viison.com>
2018-03-07 18:14:30 +0100

    Add abstract config and rules plugins

---
+++ src/PhpLint/Plugin/AbstractConfigPlugin.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpLint\Plugin;
+
+/**
+ * Extends this class in a class named 'Config' in the namespace PhpLint\Plugin\<VENDOR>\<NAME>, e.g.:
+ *
+ *  namespace PhpLint\Plugin\Acme\MyPlugin;
+ *
+ *  class Config extends PhpLint\Plugin\AbstractConfigPlugin {}
+ */
+abstract class AbstractConfigPlugin implements ConfigPlugin
+{
+    /**
+     * @inheritdoc
+     */
+    abstract public function getName(): string;
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function getExtends(): array;
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function getPlugins(): array;
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function getRules(): array;
+
+    /**
+     * @inheritdoc
+     */
+    public function getDependencies(): array
+    {
+        return array_unique(array_merge(
+            $this->getExtends(),
+            $this->getPlugins()
+        ));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->getName(),
+            'extends' => $this->getExtends(),
+            'plugins' => $this->getPlugins(),
+            'rules' => $this->getRules(),
+        ];
+    }
+}
---
+++ src/PhpLint/Plugin/AbstractRulesPlugin.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpLint\Plugin;
+
+use PhpLint\Rules\Rule;
+
+/**
+ * Extends this class in a class named 'Rules' in the namespace PhpLint\Plugin\<VENDOR>\<NAME>, e.g.:
+ *
+ *  namespace PhpLint\Plugin\Acme\MyPlugin;
+ *
+ *  class Rules extends PhpLint\Plugin\AbstractRulesPlugin {}
+ */
+abstract class AbstractRulesPlugin implements RulesPlugin
+{
+    /**
+     * @inheritdoc
+     */
+    abstract public function getName(): string;
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function getPlugins(): array;
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function getRules(): array;
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function createRule(string $ruleName): Rule;
+
+    /**
+     * @inheritdoc
+     */
+    public function getDependencies(): array
+    {
+        return $this->getPlugins();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->getName(),
+            'plugins' => $this->getPlugins(),
+            'rules' => $this->getRules(),
+        ];
+    }
+}
