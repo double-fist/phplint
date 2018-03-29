b258d952f1647fa6c1508c83bd83b49061f77690
Sven Münnich <sven.muennich@viison.com>
2018-03-29 21:49:28 +0200

    Add missing type hints

--- src/PhpLint/Ast/Token.php
+++ src/PhpLint/Ast/Token.php
@@ -5,16 +5,27 @@ namespace PhpLint\Ast;
 
 class Token
 {
+    /**
+     * @var string
+     */
     private $type;
+
+    /**
+     * @var string
+     */
     private $value;
+
+    /**
+     * @var SourceRange
+     */
     private $sourceRange;
 
     /**
-     * @param $type
-     * @param $value
-     * @param $sourceRange
+     * @param string $type
+     * @param string $value
+     * @param SourceRange $sourceRange
      */
-    public function __construct($type, $value, $sourceRange)
+    public function __construct(string $type, string $value, SourceRange $sourceRange)
     {
         $this->type = $type;
         $this->value = $value;
@@ -22,25 +33,25 @@ class Token
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getType()
+    public function getType(): string
     {
         return $this->type;
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getValue()
+    public function getValue(): string
     {
         return $this->value;
     }
 
     /**
-     * @return mixed
+     * @return SourceRange
      */
-    public function getSourceRange()
+    public function getSourceRange(): SourceRange
     {
         return $this->sourceRange;
     }
